2240506f2ff4c65407cd71d5abd0908845769854
Fang Yun <fangyuncn@gmail.com>
2019-01-04 23:19:50 +0800

    add .astylerc file

--- network.c
+++ network.c
@@ -22,25 +22,25 @@
 #define BSIZE 4096
 
 void open_log_port(char *port) {
-	pthread_t thread;
-	static struct port_info log_info = { .socket = -1 };
-	log_info.port = port;
-	open_log_connection(&log_info);
+    pthread_t thread;
+    static struct port_info log_info = { .socket = -1 };
+    log_info.port = port;
+    open_log_connection(&log_info);
 
-	/* From now on, log_info may only be modified by the single
-	 * log_thread so static allocation is ok and there is no race. */
-	pthread_create(&thread, NULL, log_thread, (void *) &log_info);
+    /* From now on, log_info may only be modified by the single
+     * log_thread so static allocation is ok and there is no race. */
+    pthread_create(&thread, NULL, log_thread, (void *) &log_info);
 }
 
 /* Open the gtp connection on the given port, redirect stdin & stdout to it. */
 void open_gtp_connection(int *socket, char *port) {
-	static struct port_info gtp_info = { .socket = -1 };
-	gtp_info.port = port;
-	int gtp_conn = open_connection(&gtp_info);
-	for (int d = STDIN; d <= STDOUT; d++) {
-		if (dup2(gtp_conn, d) < 0)
-			fail("dup2");
-	}
-	if (DEBUGL(0))
-		fprintf(stderr, "gtp connection opened\n");
+    static struct port_info gtp_info = { .socket = -1 };
+    gtp_info.port = port;
+    int gtp_conn = open_connection(&gtp_info);
+    for (int d = STDIN; d <= STDOUT; d++) {
+        if (dup2(gtp_conn, d) < 0)
+            fail("dup2");
+    }
+    if (DEBUGL(0))
+        fprintf(stderr, "gtp connection opened\n");
 }
